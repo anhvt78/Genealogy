2c98aaa7bd5dac167791cab278a5f7155e9caaa5
anhvt78 <vtanh78@gmail.com>
2026-02-08 09:44:51 +0700

    up

--- context/constants.js
+++ context/constants.js
@@ -1,61 +1,70 @@
-// import usdtJSON from "@/contracts/Contract for deploy/ABIs/usdt.json";
-import daoJSON from "@/contracts/ABIs/dao.json";
-import m2cJSON from "@/contracts/ABIs/m2c.json";
-import usdtJSON from "@/contracts/ABIs/usdt.json";
-import stackJSON from "@/contracts/ABIs/stacking.json";
+// // import usdtJSON from "@/contracts/Contract for deploy/ABIs/usdt.json";
+// import daoJSON from "@/contracts/ABIs/dao.json";
+// import m2cJSON from "@/contracts/ABIs/m2c.json";
+// import usdtJSON from "@/contracts/ABIs/usdt.json";
+// import stackJSON from "@/contracts/ABIs/stacking.json";
 
-import supplyJSON from "@/contracts/ABIs/supplier.json";
-import marketJSON from "@/contracts/ABIs/marketPlace.json";
-import productJSON from "@/contracts/ABIs/product.json";
-import productItemJSON from "@/contracts/ABIs/productItem.json";
-import arbitratorJSON from "@/contracts/ABIs/arbitrator.json";
-import saleJSON from "@/contracts/ABIs/sale.json";
-import rateJSON from "@/contracts/ABIs/rate.json";
+// import supplyJSON from "@/contracts/ABIs/supplier.json";
+// import marketJSON from "@/contracts/ABIs/marketPlace.json";
+// import productJSON from "@/contracts/ABIs/product.json";
+// import productItemJSON from "@/contracts/ABIs/productItem.json";
+// import arbitratorJSON from "@/contracts/ABIs/arbitrator.json";
+// import saleJSON from "@/contracts/ABIs/sale.json";
+// import rateJSON from "@/contracts/ABIs/rate.json";
+
+// // //USDT
+// // export const usdtAddress = "0xDa5d61E054F10AdF334974bE35d1D511ECa14458";
+// // export const usdtABI = usdtJSON.abi;
 
 // //USDT
-// export const usdtAddress = "0xDa5d61E054F10AdF334974bE35d1D511ECa14458";
+// export const usdtAddress = "0xDf2475315AD5a43acC288a10bf90514a677E210A";
 // export const usdtABI = usdtJSON.abi;
 
-//USDT
-export const usdtAddress = "0xDf2475315AD5a43acC288a10bf90514a677E210A";
-export const usdtABI = usdtJSON.abi;
+// //DAO
+// export const daoAddress = "0xb7E885f47D8695823a06716b1b59F1ec62763C9D";
+// export const daoABI = daoJSON.abi;
 
-//DAO
-export const daoAddress = "0xb7E885f47D8695823a06716b1b59F1ec62763C9D";
-export const daoABI = daoJSON.abi;
+// //M2C
+// export const m2cAddress = "0x42af9b4B85155a777735FCCc7961877C0c1864B5";
+// export const m2cABI = m2cJSON.abi;
 
-//M2C
-export const m2cAddress = "0x42af9b4B85155a777735FCCc7961877C0c1864B5";
-export const m2cABI = m2cJSON.abi;
+// //DAO
+// export const stackAddress = "0x0f0AAfd106b43298f082687534f406746aEFBA41";
+// export const stackABI = stackJSON.abi;
 
-//DAO
-export const stackAddress = "0x0f0AAfd106b43298f082687534f406746aEFBA41";
-export const stackABI = stackJSON.abi;
+// //Supply
+// export const supplyAddress = "0x89FB968EC3da865a4108000BFB409fa1AB54C770";
+// export const supplyABI = supplyJSON.abi;
 
-//Supply
-export const supplyAddress = "0x89FB968EC3da865a4108000BFB409fa1AB54C770";
-export const supplyABI = supplyJSON.abi;
+// //arbitrator
+// export const arbitratorAddress = "0xAd859Bf90Ebacc5ef77d3C0D0E35965DdE2aCB70";
+// export const arbitratorABI = arbitratorJSON.abi;
 
-//arbitrator
-export const arbitratorAddress = "0xAd859Bf90Ebacc5ef77d3C0D0E35965DdE2aCB70";
-export const arbitratorABI = arbitratorJSON.abi;
+// //Market
+// export const marketAddress = "0x5f624F88b5803F25f74E259694bDD8d89AafdC6C";
+// export const marketABI = marketJSON.abi;
 
-//Market
-export const marketAddress = "0x5f624F88b5803F25f74E259694bDD8d89AafdC6C";
-export const marketABI = marketJSON.abi;
+// //Sale
+// export const saleAddress = "0x2f3303589d32542a5b89C4D77C826e1B728665d5";
+// export const saleABI = saleJSON.abi;
 
-//Sale
-export const saleAddress = "0x2f3303589d32542a5b89C4D77C826e1B728665d5";
-export const saleABI = saleJSON.abi;
+// export const productAddress = "0x5E09e0fA10F0D8B6B72fCF6815a33118D4fdC3b3";
+// export const productABI = productJSON.abi;
 
-export const productAddress = "0x5E09e0fA10F0D8B6B72fCF6815a33118D4fdC3b3";
-export const productABI = productJSON.abi;
+// // export const productItemAddress = "0xeec5499523cda70D3A9F480BD505AC58D1E4b2Cd";
+// export const productItemABI = productItemJSON.abi;
 
-// export const productItemAddress = "0xeec5499523cda70D3A9F480BD505AC58D1E4b2Cd";
-export const productItemABI = productItemJSON.abi;
+// //Rate
+// export const rateAddress = "0x0bC33C7D50DA5d9B69aa025b2a184e8445a5047e";
+// export const rateABI = rateJSON.abi;
+// // // keccak256('LSP3ProfileName')
+// // export const _LSP4_TOKEN_NAME_KEY = 0xfbd91e2ec9c30edb1300a1e29e083807d0fb6fc70ca205a9b42b6c89124a1181;
 
-//Rate
-export const rateAddress = "0x0bC33C7D50DA5d9B69aa025b2a184e8445a5047e";
-export const rateABI = rateJSON.abi;
-// // keccak256('LSP3ProfileName')
-// export const _LSP4_TOKEN_NAME_KEY = 0xfbd91e2ec9c30edb1300a1e29e083807d0fb6fc70ca205a9b42b6c89124a1181;
+import familyNftJSON from "@/contracts/ABIs/familyNFT.json";
+import genealogyJSON from "@/contracts/ABIs/genealogy.json";
+
+export const familyNftABI = familyNftJSON.abi;
+export const familyNftAddress = "0xeec5499523cda70D3A9F480BD505AC58D1E4b2Cd";
+
+export const ABI = genealogyJSON.abi;
+export const genealogyAddress = "0xeec5499523cda70D3A9F480BD505AC58D1E4b2Cd";
--- contracts/Genealogy/genealogy.sol
+++ contracts/Genealogy/genealogy.sol
@@ -6,8 +6,8 @@ import {FamilyTypes} from "./types/FamilyTypes.sol";
 
 contract Genealogy {
 
-
     mapping(address => address) private _clanIds;
+    mapping(address => address) private _owners;
 
     event ClanCreated(address indexed nftAddress, string name);
     event ClanRemovedFromOwned(address indexed clanId);
@@ -16,19 +16,19 @@ contract Genealogy {
     function createClan(string calldata clanName, string calldata ancestorName, uint256 birthTimestamp, uint256 deathTimestamp) external {
         require(_clanIds[msg.sender] == address(0), "Clan existed");
         FamilyNFT familyNFT = new FamilyNFT(clanName, ancestorName,birthTimestamp, deathTimestamp, msg.sender);
-        address nftAddr = address(familyNFT);
-        _clanIds[msg.sender] = nftAddr;
-        
-        emit ClanCreated(nftAddr, clanName);
+        address clanId = address(familyNFT);
+        _clanIds[msg.sender] = clanId;
+        _owners[clanId] = msg.sender;
+        emit ClanCreated(clanId, clanName);
     }
 
     event ChangeClanOwner(address newOwner, address clanId);
     function changeClanOwner(address newOwner, address clanId) external {
         require(_clanIds[msg.sender] == clanId, "Only owner");
         _clanIds[newOwner] = clanId;
+        _owners[clanId] = newOwner;
         _clanIds[msg.sender] = address(0);
         FamilyNFT(payable(clanId)).transferGenealogyOwner(newOwner);
-        // FamilyNFT(payable(clanId)).transferAncestor(newOwner);
         emit ChangeClanOwner(newOwner, clanId);
     }
 
@@ -40,6 +40,10 @@ contract Genealogy {
 
     function getClanId(address owner) external view returns (address) {
         return _clanIds[owner];
+    }
+
+    function getClanOwner(address clanId) external view returns (address) {
+        return _owners[clanId];
     }
 
 }
