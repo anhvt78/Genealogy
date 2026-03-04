8706adcbb896898c7300244c610a2c9be3a6513d
anhvt78 <vtanh78@gmail.com>
2026-03-04 10:49:05 +0700

    up

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  output: "export", // Kích hoạt chế độ Static Export
   reactStrictMode: true,
   swcMinify: true,
   images: {
