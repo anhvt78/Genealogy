cc483fbc5d750f59a5a9c5b808c1cebd46621083
anhvt78 <vtanh78@gmail.com>
2026-03-06 16:18:29 +0700

    up

--- context/constants.js
+++ context/constants.js
@@ -4,6 +4,6 @@ import genealogyJSON from "./ABIs/genealogy.json";
 export const familyNftABI = familyNftJSON.abi;
 
 export const genealogyABI = genealogyJSON.abi;
-export const genealogyAddress = "0x743054f5634A33137f4f3Dad2A7311D0b0139DEE";
+export const genealogyAddress = "0xB997a5682DB8eaFd2b20C787a486C3292fafc2b1";
 
 //deployAddress: 0x898f4dfd2e5d7e5b733600892116ffd13731db51
