eb24e4dae65dd39356d7670088a6b7b57b2626ac
anhvt78 <35676757+anhvt78@users.noreply.github.com>
2026-04-08 16:01:15 +0700

    Clean up environment variable comments in config
    Removed commented-out environment variable declarations.

--- next.config.mjs
+++ next.config.mjs
@@ -14,19 +14,7 @@ const nextConfig = {
   },
   env: {
     PRIVATE_KEY: process.env.PRIVATE_KEY,
-    // INFURA_API_KEY: process.env.INFURA_API_KEY,
-    // INFURA_API_KEY_SECRET: process.env.INFURA_API_KEY_SECRET,
-    // NODE_ENV : process.env.NODE_ENV,
-    //USER_NAME : process.env.USER_NAME,
-    //DATABASE : process.env.DATABASE,
-    //DATABASE_PASSWORD : process.env.DATABASE_PASSWORD,
-    // firebase_apiKey: process.env.firebase_apiKey,
-    // firebase_authDomain: process.env.firebase_authDomain,
-    // firebase_projectId: process.env.firebase_projectId,
-    // firebase_storageBucket: process.env.firebase_storageBucket,
-    // firebase_messagingSenderId: process.env.firebase_messagingSenderId,
-    // firebase_appId: process.env.firebase_appId,
-    // firebase_databaseURL: process.env.firebase_databaseURL,
+    
   },
 };
 
