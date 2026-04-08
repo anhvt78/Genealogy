a23fd6eaf15b2dcc14d3aa015db3044050dafd50
anhvt78 <35676757+anhvt78@users.noreply.github.com>
2026-04-08 16:07:57 +0700

    Modify next.config.mjs for GitHub Pages
    Updated next.config.mjs for GitHub Pages compatibility.

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  output: "export", // Kích hoạt chế độ Static Export
-  basePath: "/Genealogy",
-  assetPrefix: "/Genealogy/",
+  output: "export", // Bắt buộc cho GitHub Pages
+  basePath: "/Genealogy", // Tên kho lưu trữ (repository name) của bạn
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ["m2c.infura-ipfs.io", "ipfs.io", "i.ibb.co"],
-    // formats: ["image/webp"],
-    unoptimized: true,
-    // domains:["localhost", "192.168.0.101",  "marketplace.cwgame.io", "ipfs.io"]
+    unoptimized: true, // Bắt buộc vì GitHub Pages không hỗ trợ Image Optimization của Next.js
   },
   env: {
     PRIVATE_KEY: process.env.PRIVATE_KEY,
-    
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
