c5c258cb23b61dbbe22bb696daadd1897940259c
anhvt78 <35676757+anhvt78@users.noreply.github.com>
2026-04-08 16:18:13 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Bắt buộc cho GitHub Pages
-  basePath: "/Genealogy", // Tên kho lưu trữ (repository name) của bạn
+  output: "export",
+  basePath: "/Genealogy",
+  trailingSlash: true, // Thêm dòng này để hỗ trợ GitHub Pages tốt hơn
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["m2c.infura-ipfs.io", "ipfs.io", "i.ibb.co"],
-    unoptimized: true, // Bắt buộc vì GitHub Pages không hỗ trợ Image Optimization của Next.js
+    domains: ["ipfs.io", "i.ibb.co"],
+    unoptimized: true,
   },
   env: {
     PRIVATE_KEY: process.env.PRIVATE_KEY,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig; // Sử dụng export default cho file .mjs
